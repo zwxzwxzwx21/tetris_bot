741589441bb56a83b7d5e5ee5ccf2f246c63b416
zwxzwxzwx21 <111466345+zwxzwxzwx21@users.noreply.github.com>
2025-03-05 18:26:18 +0100

    a lot of changes, code now is much more optimized breaking the 0.1s barrier easily, now we can find box patterns and the code is more ready to work on 6-3. i dependency check, height difference counter and some other things like uneven height approximation has been added (tyho the last one needs a lot of adjustments to work good)

--- main.py
+++ main.py
@@ -45,11 +45,11 @@ board = [
     [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
-    [' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
-    [' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
-    [' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
-    ['x','x','x','x','x','x','x','x','x','x'],
+    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
     ]
 #region
 # f - flat
@@ -252,14 +252,12 @@ def read_queue():
     p_use = pieces[pyautogui.pixel(1258, 192)]
     return p_use, queue
 
-def move_pieces(queue):
-    pass 
-# this function will work like this that you pass q as argument and make p2 -> p1, p3 -> p2 etc, can do with p2,p3 = p1,p2
 def check_pos():
     while True:
         x,y = pyautogui.position()
         print(x,y,pyautogui.pixel(x,y)) 
         time.sleep(0.2)
+
 def make_placements(board,pieces):
     pass
 
@@ -271,7 +269,6 @@ def can_place(piece,board,row,col):
                 if y >= len(board) or x < 0 or x >= len(board[0]) or board[y][x] != ' ':
                     return False
     return True
-
 
 def place_piece(piece, board, row, col):
     for dy, piece_row in enumerate(piece):
@@ -309,8 +306,6 @@ def check_holes(board):
                     return True
     return False
 
-
-
 def check_cover(board):
     # this one doesnt check for holes but if anything is above a hole, goes upwards and if there is anything covering then prints it out
     for row in range(20):
@@ -329,81 +324,106 @@ def choose_piece():
     pieces = ['I','O','S','Z','L','J','T']
     return pieces[piece_variant]
     
-def bruteforce_placements(board,pieces,current_piece_index=0):
+def bruteforce_placements(board, pieces, current_piece_index=0):
     rotations = {
-        'I': ['i_piece_f','i_piece_s'],
+        'I': ['i_piece_f', 'i_piece_s'],
         'O': ['o_piece'],
-        'S': ['s_piece_f','s_piece_s'],
-        'Z': ['z_piece_f','z_piece_s'],
-        'L': ['l_piece_f','l_piece_ccw','l_piece_cw','l_piece_180'],
-        'J': ['j_piece_f','j_piece_ccw','j_piece_cw','j_piece_180'],
-        'T': ['t_piece_f','t_piece_ccw','t_piece_cw','t_piece_180']
+        'S': ['s_piece_f', 's_piece_s'],
+        'Z': ['z_piece_f', 'z_piece_s'],
+        'L': ['l_piece_f', 'l_piece_ccw', 'l_piece_cw', 'l_piece_180'],
+        'J': ['j_piece_f', 'j_piece_ccw', 'j_piece_cw', 'j_piece_180'],
+        'T': ['t_piece_f', 't_piece_ccw', 't_piece_cw', 't_piece_180']
     }
-    # this one is keeping track of colums you move from  0 to X, where x needs to change because some pieces are longer than others
+
     column_number = {
-        'i_piece_f': 6+1,
-        'i_piece_s': 9+1,
-        'o_piece': 8+1,
-        's_piece_f' : 7+1,
-        's_piece_s' : 8+1,
-        'z_piece_f' : 7+1,
-        'z_piece_s' : 8+1,
-        'l_piece_180' : 7+1,
-        'l_piece_ccw' : 8+1,
-        'l_piece_cw' : 8+1,
-        'l_piece_f' : 7+1,
-        'j_piece_180' : 7+1,
-        'j_piece_ccw' : 8+1,
-        'j_piece_cw' : 8+1,
-        'j_piece_f' : 7+1,
-        't_piece_180' : 7+1,
-        't_piece_ccw' : 8+1,
-        't_piece_cw' : 8+1,
-        't_piece_f' : 7+1
+        'i_piece_f': 6 + 1 - 4,
+        'i_piece_s': 9 + 1 - 4,
+        'o_piece': 8 + 1 - 4,
+        's_piece_f': 7 + 1 - 4,
+        's_piece_s': 8 + 1 - 4,
+        'z_piece_f': 7 + 1 - 4,
+        'z_piece_s': 8 + 1 - 4,
+        'l_piece_180': 7 + 1 - 4,
+        'l_piece_ccw': 8 + 1 - 4,
+        'l_piece_cw': 8 + 1 - 4,
+        'l_piece_f': 7 + 1 - 4,
+        'j_piece_180': 7 + 1 - 4,
+        'j_piece_ccw': 8 + 1 - 4,
+        'j_piece_cw': 8 + 1 - 4,
+        'j_piece_f': 7 + 1 - 4,
+        't_piece_180': 7 + 1 - 4,
+        't_piece_ccw': 8 + 1 - 4,
+        't_piece_cw': 8 + 1 - 4,
+        't_piece_f': 7 + 1 - 4
     }
-    str_to_piece =  {
-        'i_piece_f' : i_piece_f,
-        'i_piece_s' : i_piece_s,
-        'o_piece' : o_piece,
+
+    str_to_piece = {
+        'i_piece_f': i_piece_f,
+        'i_piece_s': i_piece_s,
+        'o_piece': o_piece,
         's_piece_f': s_piece_f,
-        's_piece_s' :s_piece_s,
-        'z_piece_f' :z_piece_f,
-        'z_piece_s' :z_piece_s,
+        's_piece_s': s_piece_s,
+        'z_piece_f': z_piece_f,
+        'z_piece_s': z_piece_s,
         'l_piece_180': l_piece_180,
-        'l_piece_ccw' :l_piece_ccw,
-        'l_piece_cw' :l_piece_cw,
-        'l_piece_f':l_piece_f,
+        'l_piece_ccw': l_piece_ccw,
+        'l_piece_cw': l_piece_cw,
+        'l_piece_f': l_piece_f,
         'j_piece_180': j_piece_180,
-        'j_piece_ccw' :j_piece_ccw,
-        'j_piece_cw' :j_piece_cw,
-        'j_piece_f' :j_piece_f,
+        'j_piece_ccw': j_piece_ccw,
+        'j_piece_cw': j_piece_cw,
+        'j_piece_f': j_piece_f,
         't_piece_180': t_piece_180,
         't_piece_ccw': t_piece_ccw,
-        't_piece_cw' :t_piece_cw,
+        't_piece_cw': t_piece_cw,
         't_piece_f': t_piece_f,
     }
+
     if current_piece_index >= len(pieces):
-        print("Plansza po ułożeniu wszystkich klocków:")
-        #print_board(board)
-        return
-    
-    # Pobierz aktualny klocek
+        print(f"board after  {len(pieces)} pieces:")
+        print_board(board)
+        check_heights(board)
+        return True  # Znaleziono planszę
+
     current_piece = pieces[current_piece_index]
-    
-    # Przetwarzaj wszystkie możliwe ułożenia aktualnego klocka
+
     for rotation in rotations[current_piece]:
         for xpos in range(column_number[rotation]):
-            # Utwórz kopię planszy
             board_copy = copy.deepcopy(board)
-            
-            # Ułóż klocek na kopii planszy
-            drop_piece(str_to_piece[rotation], board_copy, xpos)
-            
-            # Sprawdź, czy ułożenie jest poprawne
-            if check_cover(board_copy) != True and check_holes(board_copy) != True:
-                # Rekurencyjnie przetwarzaj następny klocek
-                bruteforce_placements(board_copy, pieces, current_piece_index + 1)
 
+            drop_piece(str_to_piece[rotation], board_copy, xpos)
+
+            if check_cover(board_copy) != True and check_holes(board_copy) != True:
+                a, arr_fo_heights = height_difference(board_copy)
+                if 'I' in pieces and a < 6 and uneven_stack_est(arr_fo_heights) < 15 and not (check_i_dep(arr_fo_heights)):
+                    if bruteforce_placements(board_copy, pieces, current_piece_index + 1):
+                        return True  
+                elif 'I' not in pieces and a < 6 and uneven_stack_est(arr_fo_heights) < 15 and not (check_i_dep(arr_fo_heights)):
+                    if bruteforce_placements(board_copy, pieces, current_piece_index + 1):
+                        return True  
+
+    return False  
+
+def find_best_board(board, pieces):
+    
+    if bruteforce_placements(board, pieces):
+        print("found 6 piece sol.")
+        return
+
+   
+    if len(pieces) >= 5:
+        print("no 6 piece  looking for 5 ...")
+        if bruteforce_placements(board, pieces[:5]):
+            print("found 5 piece sol.")
+            return
+
+    if len(pieces) >= 4:
+        print("no 5 piece looking for 4...")
+        if bruteforce_placements(board, pieces[:4]):
+            print("found 4 piece sol.")
+            return
+
+    print("uhoh.")
 def check_sequence_with_gaps(pieces, sequence):
     seq_index = 0
 
@@ -413,6 +433,27 @@ def check_sequence_with_gaps(pieces, sequence):
             seq_index += 1 
             if seq_index == len(sequence):
                 return True
+    return False
+def uneven_stack_est(height_array):
+    # this one checks how unever stack is in order to remove things with i dependencies, if there is stack like 2,6,2 in heights, we dont want that so we compare how much it varies
+    # hwo it works, compare 1,2 2,3 3,4 4,5 5,6 and see the diff, sum it all up and if its quite big, fuck you ig
+    uneven_sum = 0
+    for j in range(5):
+            a = height_array[j] - height_array[j+1]
+            uneven_sum += abs(a)
+    #print("uneven height by: ", uneven_sum)
+    return uneven_sum
+
+def check_i_dep(height_arr):
+    # checks 1,2,3 2,3,4 3,4,5 4,5,6 for i dependencies, returns true if there is one and breaks to save time
+    #print(height_arr," checking i depeidencies with this heights")
+    if height_arr[1] - height_arr[0] > 2:
+        return True
+    if height_arr[4] - height_arr[5] > 2:
+        return True
+    for j in range(1, 5):  
+        if height_arr[j] < height_arr[j-1] - 2 and height_arr[j] < height_arr[j+1] - 2:
+            return True  
     return False
 
 def check_sequences_with_gaps(pieces, sequences):
@@ -433,56 +474,81 @@ def compare_to_avg(height_array,average):
         if offset_ < 0:
             offset_ *= -1
         offset += offset_
-    print(offset)
+    print('offset ', offset)
 
-def find_box(queue):
-    # this one checks if there is a box pattern possible given a queue
-    box = [[' ' for i in range(3)] for i in range(4)]
-    print(box)
 def check_heights(board):
     #this function checks how  flat the board is by taking average of every column and comparing it to each column, if its close 
     # to every single column then stack is clean, otherwise it wont be 
     
     minos_array = []
-    for stack in range(10):
+    for stack in range(6):
         minos_in_line = 0
         for y in range(20):
             
             if board[y][stack] != ' ':
                 minos_in_line += 1
-        print(minos_in_line, 'minos on line', stack)
+        #print(minos_in_line, 'minos on line', stack)
         minos_array.append(minos_in_line)
     sum_height = 0
     for height in minos_array:
         sum_height += height
-    average_height = sum_height/10
+    average_height = sum_height/6
     compare_to_avg(minos_array,average_height)
 
-if __name__ == "__main__":
-    #check_pos()
-    for i in range(1):
+def height_difference(board):
+    # this one just returns what is the height difference
+    arr = [] # it just holds values from 1-6colums heights
+    for stack in range(6):
+        minos_in_line = 0
+        for y in range(20):
+            
+            if board[y][stack] != ' ':
+                minos_in_line += 1
+        arr.append(minos_in_line)
+
+    #print(f"height diff: {max(arr) - min(arr)}")
+    return (max(arr) - min(arr)),arr
+
+def remove_box_from_q(queue, box):
+
+    #print(queue,box)
+    for piece_b in box[0]:
+        if piece_b in queue:
+            queue.remove(piece_b)
+    return queue
+
+import timeit
+import time
+
+def measure_execution_time():
+    start_time = time.perf_counter() 
+    if __name__ == "__main__":
         board1 = copy.deepcopy(board)
-        #drop_piece(s_piece_s,board1,col=i)
-        #print_board(board1)
-        #check_holes(board1)
-        #check_cover(board)
-        #piece = choose_piece()
-        piece = ['O','I','T',"O"]
-        #print(f"chosen piece is {piece}")
-        #bruteforce_placements(board1,piece)
-        #check_heights(board)
-        
+        #piece = ['O','T','I','O','S','Z'] # random queue
+        piece = ['O','T','J','L','T','O']
         found_sequences = check_sequences_with_gaps(piece, sequences)
         if found_sequences:
             print(found_sequences[-1])  # !! use that to access first sequence
-        
-        
-    #use_piece, queue = read_queue()
-    
-    #print(use_piece)
+            new_queue = remove_box_from_q(piece,found_sequences) # q without pieces in box as those would be used to make a box (wow)
+            print(new_queue, "remaining pieces to build stack on the left")
+            bruteforce_placements(board,new_queue,0)
+        else:
+            bruteforce_placements(board,piece,0)
+    end_time = time.perf_counter()  
+    execution_time = end_time - start_time  
 
-    #q_bruteforce = [use_piece] # queue which we will bruteforce on board, used as arg for function so its more flexible, can use 1 to  6 pieces
-    
-    #make_placements(board,q_bruteforce)
-    #print(board)
-    
+    print(f"runtime: {execution_time:.6f} s")  
+measure_execution_time()
+
+
+#idea:
+#mearuse up how high the stack is on both of the sides and try to match the left side with the side of the boxes on the right
+# another important thing is to have a system that ill judge the flatness of a stack and immediately pic k one which is "good enough"
+
+# todo:
+# make functionality so you can parse q and play indefinitely 
+# make line clears 
+# scoring system to judge pieces instead of bruteforcing them to save time
+# perfect clear mode/ solver
+# make so it resets when early z/s piece
+
