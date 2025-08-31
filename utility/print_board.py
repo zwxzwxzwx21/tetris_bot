5545545ca1d25ebe8106e4f03f3d45f67c8aa0a3
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2025-08-31 09:52:43 -0400

    better board printing

--- utility/print_board.py
+++ utility/print_board.py
@@ -1,6 +1,9 @@
 def print_board(board):
     # todo when bored, can use rich library and change function to print_board(color= T/F)to have colored output
-    print("===PRINTING BOARD===") # no logging cuz i think it sobv for now that its from this file 
-    print(board)
-    
+    print(
+        "===PRINTING BOARD==="
+    )  # no logging cuz i think it sobv for now that its from this file
+    print(*(" ".join(s for s in row) for row in board.T), sep="\n")
+
+
 # ye it just kinda prints the board
