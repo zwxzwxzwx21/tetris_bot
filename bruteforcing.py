32bf117fd2b5546c9aea99cbab0aff090a3c32e7
zwxzwxzwx21 <111466345+zwxzwxzwx21@users.noreply.github.com>
2025-11-10 17:51:47 +0100

    fixed some heurstic errors and wrong correct move being printed, now the bot more of less knows what is the correct move, just doesnt know how to reach it yet

--- bruteforcing.py
+++ bruteforcing.py
@@ -3,13 +3,14 @@ from logging import config
 from pprint import pp
 import time
 from board_operations.board_operations import clear_lines
-from board_operations.checking_valid_placements import can_place, soft_drop_simulation
+from board_operations.checking_valid_placements import can_place, place_piece, soft_drop_simulation
 from board_operations.stack_checking import (
     check_holes2,
     get_heights,
     uneven_stack_est,
 )
 from tetrio_parsing.calculate_attack import count_lines_clear
+from utility.print_board import print_board
 from utility.pieces import PIECES, PIECES_soft_drop
 from utility.pieces_index import PIECES_index, PIECES_xpos_indexing_value, PIECES_startpos_indexing_value
 
@@ -37,11 +38,12 @@ MAX_HEIGHT_DIFF = 6
 BRUTEFORCE_MODE = False
 import config
 values = {
-    "uneven_loss": {"default": 0.4, "max": 100},
-    "holes_punishment": {"default": 2, "max": 100},
-    "height_diff_punishment": {"default": 0.05, "max": 100},
-    "attack_bonus": {"default": 0, "max": 100},
-    "max_height_punishment": {"default": 1, "max": 100}
+    "uneven_loss": {"default": 1.5035, "max": 100},
+    "holes_punishment": {"default": 1.411334, "max": 100},
+    "height_diff_punishment": {"default": 0.08169, "max": 100},
+    "attack_bonus": {"default": 2.313, "max": 100},
+    "max_height_punishment": {"default": 0.45217, "max": 100}
+    
 }
 
 for vals, configs in values.items():
@@ -51,7 +53,16 @@ if config.PRINT_MODE:
     print(
     f"uneven_loss: {uneven_loss}, holes_punishment: {holes_punishment}, height_diff_punishment: {height_diff_punishment}, attack_bonus: {attack_bonus}" # type: ignore
 )
+
 def loss(feature: dict, uneven_loss, holes_punishment, height_diff_punishment, attack_bonus, max_height_punishment) -> float:
+    print("feature in loss function:", feature)
+    print("feature values:", feature["uneven"], feature["holes"], feature["different_heights"], feature["attack"][0], feature["max_height"])
+    print("calculating loss with values:", "uneven_loss:", uneven_loss*feature["uneven"], "holes_punishment:", holes_punishment*feature["holes"], "height_diff_punishment:", height_diff_punishment*feature["different_heights"], "attack_bonus:", attack_bonus*feature["attack"][0], "max_height_punishment:", max_height_punishment*max(feature["max_height"] - 4, 0))
+    print("uneven_loss * feature[uneven] => ",uneven_loss," * " ,feature["uneven"]," ", uneven_loss * feature["uneven"])
+    print("holes_punishment * feature[holes] => ",holes_punishment," * " ,feature["holes"]," ", holes_punishment * feature["holes"])
+    print("height_diff_punishment * feature[different_heights] => ",height_diff_punishment," * " ,feature["different_heights"]," ", height_diff_punishment * feature["different_heights"])
+    print("max_height_punishment * max(feature[max_height] - 4, 0) => ",max_height_punishment," * " ,max(feature["max_height"] - 4, 0)," ", max_height_punishment * max(feature["max_height"] - 4, 0))
+    print("attack_bonus * feature[attack][0] => ",attack_bonus," * " ,feature["attack"][0]," ", attack_bonus * feature["attack"][0])
     return (
         uneven_loss * feature["uneven"]
         + holes_punishment * feature["holes"]
@@ -69,15 +80,14 @@ def find_best_placement(board, queue, combo, stats):
     total_lines = 0
     total_attack = 0
     feature = {
-        "uneven": 0,
-        "holes": 0,
-        "height_diff": 0,
-        "max_height": 20,
-        "different_heights": 30,
-        "attack": 0,
+        "uneven": 1.5035,
+        "holes": 1.411334,
+        "height_diff": 0.08169,
+        "max_height": 0.45217,
+        "different_heights": 1,
+        "attack": 2.313,
     }
 
-    from spins_funcions import simulate_kicks
     from board_operations.checking_valid_placements import find_lowest_y_for_piece, soft_drop_simulation,get_piece_leftmost_index_from_origin,get_piece_rightmost_index_from_origin,get_piece_height,get_piece_lowest_index_from_origin,get_piece_width
             
     best_feature = feature.copy()
@@ -91,38 +101,44 @@ def find_best_placement(board, queue, combo, stats):
     for rotation_name, piece_pos_array in PIECES_index[current_piece].items():
         
         piece_width = get_piece_width(PIECES_index[current_piece][rotation_name]) 
-        print("piece width:",piece_width)
+        #print("piece width:",piece_width)
         temp_array = []
-        for start_x in range(PIECES_startpos_indexing_value[current_piece][rotation_name],11-PIECES_xpos_indexing_value[current_piece][rotation_name]):
-        #for start_x in range(7,8):
+        #for start_x in range(PIECES_startpos_indexing_value[current_piece][rotation_name],11-PIECES_xpos_indexing_value[current_piece][rotation_name]):
+        
         #for start_x in range(abs(get_piece_leftmost_index_from_origin(PIECES_index[current_piece][rotation_name])), max_start_x):
-        # TODO can do sth like this ^   
-            
+        for start_x in range(7,8):
+        # TODO can do sth like this ^  
+        # # TODO remove the lowest Y check, can do it so we keep track of piece heights, and when we place a piece on Y pos, we keep track of that
+        # from the beginning of the game,from the start lowest_y = 20, any piece will lower it and when we place piece on some Y, that makes the lowest_y lower
+        # we just overwrite lowest_y 
+            print(rotation_name," at x pos ",start_x)
             lowest_y = find_lowest_y_for_piece(PIECES_index[current_piece][rotation_name], board, start_x)
             #print("lowest y for piece at x:",start_x," is ",lowest_y)
-            for y in range(lowest_y, 20):
-                print(lowest_y,"y:",y)
-                if can_place(PIECES_index[current_piece][rotation_name], board, y, start_x):
-                    print("can place at x: ", start_x, " y: ", y)
+            for y in range(lowest_y, 20):       
+                #print(lowest_y,"y:",y)
+                if can_place(PIECES_index[current_piece][rotation_name], board, y, start_x,print_debug=False):
+                    #print("can place at x: ", start_x, " y: ", y)
                     arr_piece_info_array.append([current_piece, rotation_name, start_x, y])
                     temp_array.append([current_piece, rotation_name, start_x, y])
-            print(f"range: {PIECES_startpos_indexing_value[current_piece][rotation_name]} to {11-PIECES_xpos_indexing_value[current_piece][rotation_name]} for piece {current_piece} rotation {rotation_name}")
-            time.sleep(0.1) 
-        print(f"added {len(temp_array)} positions for rotation {rotation_name}")    
-        time.sleep(2)  
+            #print(f"range: {PIECES_startpos_indexing_value[current_piece][rotation_name]} to {11-PIECES_xpos_indexing_value[current_piece][rotation_name]} for piece {current_piece} rotation {rotation_name}")
+            #time.sleep(0.1) 
+        #print(f"added {len(temp_array)} positions for rotation {rotation_name}")    
+        #.sleep(2)  
     print("total positions to try:",len(arr_piece_info_array))
+    time.sleep(1)
 
 
-    time.sleep(2137)
+    
     for position_info in arr_piece_info_array:
-        new_board = simulate_kicks(board, arr_piece_info_array)
-        if new_board is None:
+        #new_board = simulate_kicks(board, arr_piece_info_array)
+        new_board, is_place_piece_successful = place_piece(PIECES_index[position_info[0]][position_info[1]],position_info[0], board, position_info[2], position_info[3], print_debug=False)
+        if not is_place_piece_successful:
             if config.PRINT_MODE:
                 print(f"GAMEOVER, score (best loss) : {best_loss} ")
             GAMEOVER = True
             break
         board_after_clear, cleared_lines = clear_lines(new_board)
-
+        # piece info array example ("T",'flat_0',x(fore xample 4),y(for example 15))
         heights = get_heights(board_after_clear)
         feature["max_height"] = max(heights)
         feature["uneven"] = int(uneven_stack_est(heights))
@@ -131,15 +147,18 @@ def find_best_placement(board, queue, combo, stats):
             heights[x] != heights[x + 1] for x in range(len(heights) - 1)
         )
         feature["attack"] = count_lines_clear(cleared_lines, combo, board_after_clear)
-
+        print_board(board_after_clear)
+        print(position_info)
         if (current_loss := loss(feature, uneven_loss, holes_punishment,  # type: ignore
                                 height_diff_punishment, attack_bonus, max_height_punishment)) < best_loss:# type: ignore
-            best_move = f"{current_piece}_x{start_x}_{rotation_name}"
+            #best_move = f"{current_piece}_x{start_x}_{rotation_name}"
+            best_move = f"{position_info[0]}_x{position_info[2]}_{position_info[1]}"
             move_history = [best_move]
             best_loss = current_loss
             best_feature = feature
             total_attack += feature["attack"][0]
             total_lines += cleared_lines
+            print("UPDATED BEST MOVE TO:", best_move, " with loss: ", best_loss)
             
     if config.PRINT_MODE:
         pp(best_feature)
@@ -157,4 +176,5 @@ def find_best_placement(board, queue, combo, stats):
             
         return None
     assert move_history
+    print(f"best move: {best_move} with loss: {best_loss}")
     return move_history, best_move
